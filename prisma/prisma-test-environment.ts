412e31a4ffe8be9b673365ac6955aed1f31a8188
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2023-12-27 18:22:39 -0300

    add tests

---
+++ prisma/prisma-test-environment.ts
@@ -0,0 +1,43 @@
+import dotenv from 'dotenv'
+
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+import NodeEnvironment from 'jest-environment-node'
+import type { JestEnvironmentConfig, EnvironmentContext } from '@jest/environment'
+import { PrismaClient } from '@prisma/client'
+
+dotenv.config({ path: '.env.testing' })
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL (schema: string): string {
+  if (process.env.DATABASE_URL === undefined) {
+    throw new Error('Please provide a DATABASE_URL environment variable in file .env.testing')
+  }
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', schema)
+  return url.toString()
+}
+
+export default class PrismaTestEnvironment extends NodeEnvironment {
+  private readonly schema: string
+
+  constructor (config: JestEnvironmentConfig, context: EnvironmentContext) {
+    super(config, context)
+    this.schema = `test_${randomUUID()}`
+  }
+
+  async setup (): Promise<void> {
+    const databaseURL = generateDatabaseURL(this.schema)
+    process.env.DATABASE_URL = databaseURL
+    this.global.process.env.DATABASE_URL = databaseURL
+    execSync('npx prisma migrate deploy')
+  }
+
+  async teardown (): Promise<void> {
+    await prisma.$executeRawUnsafe(
+      `DROP SCHEMA IF EXISTS "${this.schema}" CASCADE`
+    )
+    await prisma.$disconnect()
+  }
+}
