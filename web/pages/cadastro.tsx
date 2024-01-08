77ce5e93b9d7f37cb89df5ffe547deb7e3a757c6
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2024-01-08 01:33:00 +0000

    start request

--- web/pages/cadastro.tsx
+++ web/pages/cadastro.tsx
@@ -1,28 +1,79 @@
 import Link from "next/link";
+import { useState } from "react";
 
-export default function Home() {
+export default function Cadastro() {
+  const [formData, setFormData] = useState({
+    name: "",
+    user: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const { confirmPassword, name, password, user } = formData;
+    console.log(confirmPassword, name, password, user)
+   // const response = await fetch('/hello', { method: 'get' })
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
+
   return (
     <main className="h-screen flex items-center justify-center text-center">
       <div className="bg-black-800 w-96 p-4 rounded-md">
         <h1 className="text-purple-900 font-bold text-2xl">
           CADASTRO
         </h1>
-        <form className='flex flex-col justify-center items-center content-center'>
+        <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center content-center'>
           <div className="flex flex-col text-start text-white mt-4">
             <label className="mb-1 font-bold">Nome Completo</label>
-            <input className="bg-black-700 rounded w-72 h-9 p-2" type="text" />
+            <input
+              required 
+              className="bg-black-700 rounded w-72 h-9 p-2" 
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
           </div>
           <div className="flex flex-col text-start text-white mt-4">
             <label className="mb-1 font-bold">Usuário</label>
-            <input className="bg-black-700 rounded w-72 h-9 p-2" type="text" />
+            <input
+              required 
+              className="bg-black-700 rounded w-72 h-9 p-2"
+              type="text" 
+              name="user"
+              value={formData.user}
+              onChange={handleChange}
+            />
           </div>
           <div className="flex flex-col text-start text-white mt-4">
             <label className="mb-1 font-bold">Senha</label>
-            <input className="bg-black-700 rounded w-72 h-9 p-2" type="text" />
+            <input
+              required 
+              className="bg-black-700 rounded w-72 h-9 p-2"
+              type="password" 
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
           </div>
           <div className="flex flex-col text-start text-white mt-4">
             <label className="mb-1 font-bold">Confirmar Senha</label>
-            <input className="bg-black-700 rounded w-72 h-9 p-2" type="text" />
+            <input
+              required 
+              className="bg-black-700 rounded w-72 h-9 p-2"
+              type="password" 
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+            />
             <span className='text-start text-white text-sm mt-1'>
               Já é membro? {' '}
               <Link 
@@ -35,7 +86,8 @@ export default function Home() {
           </div>
           <button 
             className="bg-blue text-white p-2 rounded-lg mt-8 w-40 text-lg hover:bg-blue-700 font-bold py-2 px-4 rainbow-hover"
-            type="submit">
+            type="submit"
+          >
             Enviar
           </button>
         </form>
