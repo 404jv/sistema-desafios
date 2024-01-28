f3a68a8a1e4e318b40076804f041c7132191b062
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2024-01-28 19:59:52 -0300

    add seed admin

---
+++ backend/src/database/seed/admin.ts
@@ -0,0 +1,20 @@
+import { prisma } from '@/database/prisma'
+import 'dotenv'
+import { hashSync } from 'bcrypt'
+
+const password = process.env.ADMIN_PASSWORD ?? '123'
+
+async function createAdmin (): Promise<void> {
+  console.log(password)
+  await prisma.user.create({
+    data: {
+      name: 'adminSys',
+      password: hashSync(password, 10),
+      username: 'adminSys',
+      isAdmin: true
+    }
+  })
+}
+
+createAdmin()
+  .catch(error => { console.log(error) })
